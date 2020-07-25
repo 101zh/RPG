27a0b9c776b43e085ac2c10fcdce44e44f331bf4
Hunter Zhuang <hunterzhuang0@gmail.com>
2020-07-25 12:48:57 -0700

    made an object in python

--- test_notes.py
+++ test_notes.py
@@ -4,3 +4,13 @@ numbers = [1,2,3,4]
 # stepping in slicing
 print(letters[::2]) # ac
 print(numbers[::2]) # [1, 3]
+
+class Tree:
+    def __init__(self, treetype, height):
+        self.treetype = treetype
+        self.height = height
+
+pinetree1 = Tree("pine", 1000)
+print(pinetree1)
+print(pinetree1.treetype)
+print(pinetree1.height)
