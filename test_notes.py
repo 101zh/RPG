4ebf8cb6ce209669b63a3c869c1861d6514e85e1
Hunter Zhuang <hunterzhuang0@gmail.com>
2020-07-30 21:04:52 -0700

    added more notes about objects

--- test_notes.py
+++ test_notes.py
@@ -5,12 +5,16 @@ numbers = [1,2,3,4]
 print(letters[::2]) # ac
 print(numbers[::2]) # [1, 3]
 
-class Tree:
+class Tree: #class is used to make an object
     def __init__(self, treetype, height):
-        self.treetype = treetype
+        self.treetype = treetype #self refers to the object
         self.height = height
 
 pinetree1 = Tree("pine", 1000)
 print(pinetree1)
-print(pinetree1.treetype)
+print(pinetree1.treetype)#treetype is a field or a proporty of an object
 print(pinetree1.height)
+
+# __name__ == "__main__" when run python test_notes.py
+# __name__=="test_notes" when run from another file
+print('test_notes:', __name__)
