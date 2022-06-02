c389f2bbcb4cc120728cce61888979dca95a6b96
Hunter Zhuang <67253838+101zh@users.noreply.github.com>
2022-06-02 19:02:29 +0000

    changed shop display a bit

--- shop.py
+++ shop.py
@@ -19,21 +19,21 @@ def shop(player):
             print("\nArmor:")
             for i in shoplist:
                 if i.buyable==True and (i.typeofitem=="helmet" or i.typeofitem=="chestplate" or i.typeofitem=="boots" or i.typeofitem=="leggings"):
-                     print(str(i.name)+"  $"+i.cost)
+                     print(str(i.cost)+"$    "+str(i.name))
             shopinput=input("\nWhat do you want to buy? ")
             player.buy(shopinput)
         elif shopinput[:1]=="w":
             print("\nWeapons:")
             for i in shoplist:
                 if i.typeofitem=="weapon" and i.buyable==True:
-                    print(str(i.name)+"  $"+i.cost)
+                    print(str(i.cost)+"$    "+str(i.name))
             shopinput=input("\nWhat do you want to buy? ")
             player.buy(shopinput)
         elif shopinput[:1]=="o":
             print("\nOther Items:")
             for i in shoplist:
                 if i.typeofitem=="usable" and i.buyable==True:
-                     print(str(i.name)+"  $"+i.cost)
+                    print(str(i.cost)+"$    "+str(i.name))
             shopinput=input("\nWhat do you want to buy? ")
             player.buy(shopinput)
         elif shopinput[:1]=="l":
