b54d08fd8b4b8841e7fd177141104a10dd241618
101zh <>
2022-06-09 19:33:17 +0000

    Finished base shop
    -need to create battle func
    -would like to create slayers

--- shop.py
+++ shop.py
@@ -27,8 +27,8 @@ def shop(player):
         shopinput = input("What would you like to buy? ").lower()
         if shopinput[:1]=="a":
             pages=inputcheck("Which page do you want to go to? (1-2 pages) ")
-            print("\nArmor:")
             pages*=4
+            print("\nArmor:")
             for i in shoplist:
                 if i.buyable==True and (i.typeofitem=="helmet" or i.typeofitem=="chestplate" or i.typeofitem=="boots" or i.typeofitem=="leggings"):
                     templist.append(i)
@@ -37,20 +37,35 @@ def shop(player):
                 if armor<pages and armor>pages-5:
                     print(str(templist[armor].cost)+"$    "+str(templist[armor].name))
             shopinput=input("\nWhat do you want to buy?(type leave to not buy) ")
+            templist=[]
             player.buy(shopinput)
         elif shopinput[:1]=="w":
+            pages=inputcheck("Which page do you want to go to? (1 pages) ")
+            pages*=4
             print("\nWeapons:")
             for i in shoplist:
                 if i.typeofitem=="weapon" and i.buyable==True:
-                    print(str(i.cost)+"$    "+str(i.name))
+                    templist.append(i)
+                    tempamount=range(len(templist))        
+            for weapon in tempamount:
+                if weapon<pages and weapon>pages-5:
+                    print(str(templist[weapon].cost)+"$    "+str(templist[weapon].name))
             shopinput=input("\nWhat do you want to buy? (type leave not buy) ")
+            templist=[]
             player.buy(shopinput)
         elif shopinput[:1]=="o":
+            pages=inputcheck("Which page do you want to go to? (1 pages) ")
+            pages*=4
             print("\nOther Items:")
             for i in shoplist:
                 if i.typeofitem=="usable" and i.buyable==True:
-                    print(str(i.cost)+"$    "+str(i.name))
+                    templist.append(i)
+                    tempamount=range(len(templist))        
+            for usable in tempamount:
+                if usable<pages and usable>pages-5:
+                    print(str(templist[usable].cost)+"$    "+str(templist[usable].name))
             shopinput=input("\nWhat do you want to buy? (type leave not buy) ")
+            templist=[]
             player.buy(shopinput)
         elif shopinput[:1]=="l":
           print("Bye! Come back again!\n")
