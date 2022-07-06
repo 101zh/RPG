56eaae60f4c6063da3e1fb06efba6b96f98a6051
Hunter Zhuang <hunterzhuang0@gmail.com>
2022-07-06 15:33:55 -0700

    added areas object

---
+++ areas.py
@@ -0,0 +1,23 @@
+areaDict={}
+class area:
+    def __init__(self,name:str, areanum:int, monsters:list, shopitems:list):
+        self.name=name
+        self.areanum=areanum
+        self.monsters=monsters
+        self.shopitems= shopitems
+        areaDict[self.areanum]=self
+
+    # Makes the monsters inside monsters:list actual characters
+    def init(self,monlist:list,itemDict:dict):
+        self.monsters.clear()
+        for i in monlist:
+            if i.area.areanum==self.areanum:
+                self.monsters.append(i)
+        for key, data in itemDict:
+            if data.area.areanum==self.areanum:
+                self.shopitems.append(data)
+
+
+area("Null", 1, [],[])
+area("Green Hills", 1, [],[])
+
